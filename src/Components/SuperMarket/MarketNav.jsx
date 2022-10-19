074066962f768e4339570c68b6403e5bd4a6d4cf
Tal <trimoni8@gmail.com>
2022-10-19 18:16:57 -0400

    toggleclick

--- src/Components/SuperMarket/MarketNav.jsx
+++ src/Components/SuperMarket/MarketNav.jsx
@@ -1,14 +1,20 @@
-import CategoryMenu from './CategoryMenu'
+import CategoryMenu from "./CategoryMenu";
 
 const MarketNav = (props) => {
-  console.log(props.product)
+  console.log(props.product);
   return (
     <nav>
       <h1>Super Market</h1>
-      <CategoryMenu products={props.products} setProductCategory={props.setProductCategory}/>
-      <button id="cart-button"></button>
+      <CategoryMenu
+        products={props.products}
+        setProductCategory={props.setProductCategory}
+      />
+      <button
+        id="cart-button"
+        onClick={() => props.setToggleCart(!props.toggleCart)}
+      ></button>
     </nav>
-  )
-}
+  );
+};
 
-export default MarketNav
+export default MarketNav;
--- src/Components/SuperMarket/SuperMarket.jsx
+++ src/Components/SuperMarket/SuperMarket.jsx
@@ -1,48 +1,64 @@
-import '../../styles/super-market.css'
-import MarketNav from './MarketNav'
-import DisplayProducts from './DisplayProducts'
-import Cart from './Cart'
-import { useState } from 'react'
+import "../../styles/super-market.css";
+import MarketNav from "./MarketNav";
+import DisplayProducts from "./DisplayProducts";
+import Cart from "./Cart";
+import { useState } from "react";
 
 // Components & Data
-import { products } from '../../data/market-data'
-console.log(products)
+import { products } from "../../data/market-data";
+console.log(products);
 const SuperMarket = (props) => {
+  const [cart, setCart] = useState([]);
 
-  const [cart, setCart] = useState([])
+  const [toggleCart, setToggleCart] = useState(true);
 
-  const [productCatergory, setProductCategory] = useState('Produce')
-  console.log(productCatergory)
+  const [productCatergory, setProductCategory] = useState("Produce");
+  console.log(productCatergory);
 
-  console.log('Imported product data:::', products)
+  console.log("Imported product data:::", products);
 
   const addToCart = (item) => {
-    setCart([{ ...item, quantity: 1 }, ...cart])
-    console.log('addToCart works!', item)
-  }
+    setCart([{ ...item, quantity: 1 }, ...cart]);
+    console.log("addToCart works!", item);
+  };
 
   const removeFromCart = (item) => {
     if (item.quantity > 1) {
-      setCart(cart.map((prod) => prod.id === item.id
-        ? { ...item, quantity: item.quantity - 1 }
-        : prod
-      ))
+      setCart(
+        cart.map((prod) =>
+          prod.id === item.id ? { ...item, quantity: item.quantity - 1 } : prod
+        )
+      );
     } else {
-      setCart(cart.filter((prod) => prod.id !== item.id))
+      setCart(cart.filter((prod) => prod.id !== item.id));
     }
-  }
+  };
 
   return (
     <div className="super-market">
       <section>
-        <MarketNav products={products} setProductCategory={setProductCategory}/>
-        <DisplayProducts products={products} productCategory={productCatergory} addToCart={addToCart}/>
+        <MarketNav
+          products={products}
+          setProductCategory={setProductCategory}
+          toggleCart={toggleCart}
+          setToggleCart={setToggleCart}
+        />
+        <DisplayProducts
+          products={products}
+          productCategory={productCatergory}
+          addToCart={addToCart}
+        />
       </section>
-
-      <Cart cart={cart} removeFromCart={removeFromCart} setCart={setCart} handleExchange={props.handleExchange}/>
-
+      {toggleCart && (
+        <Cart
+          cart={cart}
+          removeFromCart={removeFromCart}
+          setCart={setCart}
+          handleExchange={props.handleExchange}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SuperMarket
+export default SuperMarket;
